f3a9e1cd0619fd570177a2c4473caa94492f46d6
Kevin P. Dyer <kpdyer@gmail.com>
2013-10-14 19:06:50 -0400

    Issue #30: Removal of hard-coded build.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.core import Extension
+
+fte_cRegex = Extension('fte.cRegex',
+                       include_dirs=['fte'],
+                       libraries=['gmp',
+                                  'boost_python',
+                                  'boost_system'],
+                       sources=['fte/cRegex.cc'])
+
+setup(name='Format-Transforming Encrypion (FTE)',
+      version='0.2.0-alpha',
+      description='FTE',
+      author='Kevin P. Dyer',
+      author_email='kpdyer@gmail.com',
+      url='https://github.com/redjack/FTE',
+      packages=['fte', 'fte.tcp'],
+      scripts=['bin/fte_proxy'],
+      ext_modules=[fte_cRegex],
+      )
