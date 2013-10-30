ab5051eae22ab59136ee26d50f4e385e0ab60d4a
Kevin P. Dyer <kpdyer@gmail.com>
2013-10-29 19:23:36 -0700

    Issue #24: Better readability for fte.conf

--- fte/conf.py
+++ fte/conf.py
@@ -34,47 +34,61 @@ def module_path():
 
 conf = {}
 
+
 """The base path for the location of the fte.* modules."""
 conf['general.base_dir'] = os.path.abspath(os.path.join(module_path()))
+
 
 """The location that we store *.pid files, such that we can kill FTE from the command line."""
 conf['general.pid_dir'] = tempfile.gettempdir()
 
+
 """Our runtime mode: client|server|test"""
 conf['runtime.mode'] = None
 
+
 """The maximum number of queued connections for sockets"""
 conf['runtime.fte.relay.backlog'] = 5
+
 
 """Our client-side ip:port to listen for incoming connections"""
 conf['runtime.client.ip'] = '127.0.0.1'
 conf['runtime.client.port'] = 8079
 
+
 """Our server-side ip:port to listen for connections from FTE clients"""
 conf['runtime.server.ip'] = '127.0.0.1'
 conf['runtime.server.port'] = 8080
+
 
 """Our proxy server, where the FTE server forwards outgoing connections."""
 conf['runtime.proxy.ip'] = '127.0.0.1'
 conf['runtime.proxy.port'] = 8081
 
+
 """The default socket timeout."""
 conf['runtime.fte.relay.socket_timeout'] = 0.001
+
 
 """The maxium number of bytes to sgement for an outgoing message."""
 conf['runtime.fte.record_layer.max_cell_size'] = 2 ** 16
 
+
 """The default client-to-server language."""
 conf['runtime.state.upstream_language'] = 'manual-http-request'
+
 
 """The default server-to-client language."""
 conf['runtime.state.downstream_language'] = 'manual-http-response'
 
+
 """The default AE scheme key."""
 conf['runtime.fte.encrypter.key'] = 'FF' * 16 + '00' * 16
 
+
 """The default max_len parameter to use for buildTable."""
 conf['fte.default_max_len'] = 512
+
 
 """The default definitions file to use."""
 conf['fte.defs.release'] = '20131023'
