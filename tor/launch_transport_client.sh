f0ddb513a25bb8595c9103a3287baf7f112bc652
Kevin P. Dyer <kpdyer@gmail.com>
2013-11-15 12:27:40 -0800

    Issue #37: pyptlib integration, startup scripts to launch client/server

---
+++ tor/launch_transport_client.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo pkill -9 -f fteproxy
+sudo pkill -9 -f tor
+
+export TOR_PT_STATE_LOCATION=/tmp
+export TOR_PT_MANAGED_TRANSPORT_VER=1
+export TOR_PT_CLIENT_TRANSPORTS=fte
+
+fteproxy --mode client --managed
---
+++ tor/launch_transport_server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo pkill -9 -f fteproxy
+sudo pkill -9 -f tor
+
+export TOR_PT_STATE_LOCATION=/tmp
+export TOR_PT_MANAGED_TRANSPORT_VER=1
+export TOR_PT_CLIENT_TRANSPORTS=fte
+export TOR_PT_EXTENDED_SERVER_PORT=
+export TOR_PT_ORPORT=127.0.0.1:8081
+export TOR_PT_SERVER_BINDADDR=fte-0.0.0.0:8080
+export TOR_PT_SERVER_TRANSPORTS=fte
+
+fteproxy --mode server --managed
