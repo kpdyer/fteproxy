aa992e6074513df19b92a3eac9e23d7faa1ae905
Kevin P. Dyer <kpdyer@dyer.io>
2026-01-10 15:33:32 -0800

    Add PyPI publishing workflow and dedicated README
    New files:
    - PYPI_README.md: Concise README for PyPI package page
    - pyproject.toml: Modern Python packaging configuration
    - .github/workflows/publish.yml: PyPI publishing workflow
    
    The publish workflow:
    - Triggers on GitHub releases (publishes to PyPI)
    - Can be manually triggered for TestPyPI or PyPI
    - Uses trusted publishing (OIDC) for secure uploads
    - Builds and validates package before publishing
    
    Updated:
    - setup.py: Cleaned up, uses PYPI_README.md
    - MANIFEST.in: Include new files in distribution

---
+++ PYPI_README.md
@@ -0,0 +1,58 @@
+# fteproxy
+
+[![Tests](https://github.com/kpdyer/fteproxy/actions/workflows/tests.yml/badge.svg)](https://github.com/kpdyer/fteproxy/actions/workflows/tests.yml)
+[![PyPI version](https://badge.fury.io/py/fteproxy.svg)](https://badge.fury.io/py/fteproxy)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Python 3.8+](https://img.shields.io/badge/python-3.8+-blue.svg)](https://www.python.org/downloads/)
+
+**fteproxy** provides transport-layer protection to resist keyword filtering, censorship, and discriminatory routing policies using Format-Transforming Encryption (FTE).
+
+## Installation
+
+```bash
+pip install fteproxy
+```
+
+### System Dependencies
+
+**Ubuntu/Debian:**
+```bash
+sudo apt-get install libgmp-dev
+```
+
+**macOS:**
+```bash
+brew install gmp
+```
+
+## Quick Start
+
+### Server
+
+```bash
+fteproxy --mode server --server_ip 0.0.0.0 --server_port 8080 --proxy_ip 127.0.0.1 --proxy_port 8081
+```
+
+### Client
+
+```bash
+fteproxy --mode client --client_ip 127.0.0.1 --client_port 8079 --server_ip <server-ip> --server_port 8080
+```
+
+## How It Works
+
+```
+[Application] <-> [fteproxy client] <--FTE encoded--> [fteproxy server] <-> [Destination]
+```
+
+fteproxy encodes traffic to match user-specified regular expressions, making it appear as allowed traffic (e.g., HTTP) to network filters.
+
+## Links
+
+- **Documentation:** https://github.com/kpdyer/fteproxy
+- **Homepage:** https://fteproxy.org
+- **Publication:** [Protocol Misidentification Made Easy with Format-Transforming Encryption](https://kpdyer.com/publications/ccs2013-fte.pdf) (CCS 2013)
+
+## License
+
+MIT License
--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-from setuptools import Extension
-
 import glob
-import sys
-import os
 
 with open('fteproxy/VERSION') as fh:
     FTEPROXY_RELEASE = fh.read().strip()
@@ -14,42 +10,49 @@ package_data_files = []
 package_data_files += ['VERSION']
 for filename in glob.glob('fteproxy/defs/*.json'):
     jsonFile = filename.split('/')[-1]
-    package_data_files += ['defs/'+jsonFile]
+    package_data_files += ['defs/' + jsonFile]
 package_data = {'fteproxy': package_data_files}
 
-with open('README.md', encoding='utf-8') as file:
+with open('PYPI_README.md', encoding='utf-8') as file:
     long_description = file.read()
 
-setup(name='fteproxy',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      test_suite='fteproxy.tests.suite',
-      package_data=package_data,
-      version=FTEPROXY_RELEASE,
-      description='fteproxy',
-      author='Kevin P. Dyer',
-      author_email='kpdyer@gmail.com',
-      url='https://fteproxy.org/',
-      packages=['fteproxy', 'fteproxy.defs', 'fteproxy.tests'],
-      install_requires=['fte'],
-      python_requires='>=3.8',
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'License :: OSI Approved :: MIT License',
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Topic :: Security :: Cryptography',
-          'Topic :: Internet :: Proxy Servers',
-          'Operating System :: OS Independent',
-      ],
-      entry_points = {
+setup(
+    name='fteproxy',
+    version=FTEPROXY_RELEASE,
+    description='Format-Transforming Encryption proxy for censorship circumvention',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Kevin P. Dyer',
+    author_email='kpdyer@gmail.com',
+    url='https://github.com/kpdyer/fteproxy',
+    project_urls={
+        'Homepage': 'https://fteproxy.org',
+        'Documentation': 'https://github.com/kpdyer/fteproxy',
+        'Source': 'https://github.com/kpdyer/fteproxy',
+        'Bug Tracker': 'https://github.com/kpdyer/fteproxy/issues',
+    },
+    packages=['fteproxy', 'fteproxy.defs', 'fteproxy.tests'],
+    package_data=package_data,
+    install_requires=['fte'],
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Internet :: Proxy Servers',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
         'console_scripts': [
             'fteproxy = fteproxy.cli:main'
-            ]
-        },
-      )
+        ]
+    },
+    keywords='fte, encryption, proxy, censorship, privacy',
+)
